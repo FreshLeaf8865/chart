97c37bf411a077a3f908dcc432d13a1bbc464c4d
FreshLeaf8865 <cmd.bsolution@outlook.com>
2018-08-01 09:14:26 -0700

    date-picker, displaying chart upon selecting table

--- js/main.js
+++ js/main.js
@@ -1,77 +1,112 @@
 
-var data_attendaces = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33];
-var data_forecast = [5, 5, 5, 5, 5, 6, 7, 7, 8, 8, 8, 10, 10, 12, 15, 16, 19, 19, 22, 22, 20, 16, 15, 10];
-var data_breaches = [2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5];
-
-new Chart(document.getElementById("bar-chart"), {
-	type: 'bar',
-	data: {
-		labels: [
-			"01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", 
-			"13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"
-		],
-		datasets: [
-			{
-				label: "Attendaces",
-				backgroundColor: "#3e95cd",
-				data: data_attendaces
-			},
-			{
-				label: "Forecast",
-				backgroundColor: "#8e5ea2",
-				data: data_forecast
-			},
-			{
-				label: "Breaches",
-				backgroundColor: "#45da78",
-				data: data_breaches
-			}
-		]
-	},
-	options: {
-		title: {
-			display: true,
-			text: 'Status'
-		}
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
 	}
-});
+	return color;
+}
+
+var chart = null;
+
+function draw_chart(date, tableNumber) {
+	if (typeof data[date] == 'undefined') {
+		$("#bar-chart").hide();
+		$(".message").show();
+		return;
+	}
+
+	$("#bar-chart").show();
+	$(".message").hide();
+
+	var datasets = [];
+	var chartData = data[date][tableNumber];
+	var keys = Object.keys(chartData);
+
+	for (let i in keys) {
+		datasets.push({
+			label: keys[i],
+			backgroundColor: getRandomColor(),
+			data: chartData[keys[i]]
+		});
+	}
+
+	if (chart && typeof chart === "object") {
+		chart.destroy();
+	}
+
+	chart = new Chart(document.getElementById("bar-chart"), {
+		type: 'bar',
+		data: {
+			labels: [
+				"01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", 
+				"13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"
+			],
+			datasets: datasets
+		},
+		options: {
+			title: {
+				display: false,
+				text: 'Status'
+			}
+		}
+	});
+}
+
+function draw_table(date) {
+	if (typeof data[date] == 'undefined') {
+		$(".table-wrapper").html("");
+		return;
+	}
+
+	var td_width = ($("#bar-chart").width() - 50) / 24;
+
+	var table = "";
+
+	for (let i in data[date]) {
+		table += "<table onClick=\"draw_chart('" + date + "', " + i + ")\"><tbody>";
+		var keys = Object.keys(data[date][i]);
+
+		for (let j in keys) {
+			table += "<tr>";
+			table += "<td style='width: 100px;'>" + keys[j] + "</td>";
+
+			for (let k in data[date][i][keys[j]]) {
+				table += "<td style='text-align: center; width: " + td_width + "px;'>" + data[date][i][keys[j]][k] + "</td>";
+			}
+
+			table += "</tr>";
+		}
+
+		table += "</tbody></table>";
+	}
+
+	$(".table-wrapper").html(table);
+}
 
 $(document).ready(function() {
 
-	function draw_table() {
-		var td_width = ($("#bar-chart").width() - 50) / 24;
-
-		var tr_attendaces = "<tr>";
-		var tr_forecast = "<tr>";
-		var tr_breaches = "<tr>";
-
-		tr_attendaces += "<td style='width: 100px;'>Attendaces</td>";
-		tr_forecast += "<td style='width: 100px;'>Forecast</td>";
-		tr_breaches += "<td style='width: 100px;'>Breaches</td>";
-
-		for (let i in data_attendaces) {
-			tr_attendaces += "<td style='text-align: center; width: " + td_width + "px;'>" + data_attendaces[i] + "</td>";
-		}
-		for (let i in data_forecast) {
-			tr_forecast += "<td style='text-align: center;'>" + data_forecast[i] + "</td>";
-		}
-		for (let i in data_breaches) {
-			tr_breaches += "<td style='text-align: center;'>" + data_breaches[i] + "</td>";
-		}
-
-		tr_attendaces += "</tr>";
-		tr_forecast += "</tr>";
-		tr_breaches += "</tr>";
-
-		var total_html = tr_attendaces + tr_forecast + tr_breaches;
-
-		$("#bar-chart-values tbody").html(total_html);
-	}
-
-	draw_table();
-
-	$(window).resize(function() {
-		draw_table();
+	$("#date-picker").datepicker({
+		onSelect: function(selectedDate) {
+			draw_chart(selectedDate, 0);
+			draw_table(selectedDate);
+		},
+		dateFormat: "yy-mm-dd"
 	});
+
+	$("#date-picker").datepicker().datepicker("setDate", "today");
+
+	var d = new Date();
+	var month = d.getMonth() + 1;
+	var day = d.getDate();
+	var today = d.getFullYear() + "-" + (month < 10 ? '0' : '') + month + "-" + (day < 10 ? '0' : '') + day;
+
+	draw_chart(today, 0);
+	draw_table(today);
+
+	// $(window).resize(function() {
+	// 	draw_table();
+	// });
 
 });
